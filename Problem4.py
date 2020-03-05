c77a96a0fd1da1c7594498b04971462c6abeea9c
Saurabh Mudgal <smsaurabhmudgal@gmail.com>
2020-03-05 15:29:12 +0800

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -4,19 +4,16 @@ var2 = 0
 var3 = 0
 
 # Nested loop to carry out the multiple of every single 3 digit integer upto 1000 and store the resulting palindromes in a list
-for var1 in range(1000):
-  
-  for var2 in range(1000):
+for var1 in range(100, 1000, 1):
+  for var2 in range(100, 1000, 1):
     var3 = var1*var2
     if str(var3) == str(var3)[::-1] and var3 != 0:
       palindrome_list.append(var3)
-      
     var2 = var2+1
-  
   var1 = var1+1
   var2 = 0
 
-# arrange the list of palindrome in ascending order
+# Arrange the list of palindrome in ascending order
 palindrome_list.sort()
 
 # To print the largest palindrome possible. List starts counting from 0. Hence '-1' gives the last entry in list; which, after arranging the list in ascending order, is the largest Palindrome.
